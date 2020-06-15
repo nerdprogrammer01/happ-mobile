51f7a3a4fd043002ecc464e1cc1a67c53ad87fb9
CNwadike <chimanwadike94@gmail.com>
2020-06-15 18:02:13 +0100

    confirm appointment

--- src/screens/appointment/ConfirmAppointmentScreen.tsx
+++ src/screens/appointment/ConfirmAppointmentScreen.tsx
@@ -3,20 +3,33 @@ import { View, Text, Picker, StyleSheet, TextInput } from 'react-native';
 import { Theme } from "../../theme";
 import { Button } from "../../components/buttons";
 import { useLocalization } from "../../localization";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation,useRoute } from "@react-navigation/native";
+import {NewAppointmentModel} from "../../models/NewAppointmentModel";
 
 type TProps = {};
 
 export const ConfirmAppointmentScreen: React.FC<TProps> = props => {
     const [selectedValue, setSelectedValue] = useState("java");
     const navigation = useNavigation();
+    const route = useRoute()
     const { getString } = useLocalization();
+    let appointmentModel = JSON.parse(route.params["appointmentModel"]) as NewAppointmentModel;
+
     return (
         <View style={styles.container}>
-            
+           <Text style={styles.titleText}>Selected Doctor</Text>
+         
+          <TextInput
+                    style={styles.input}
+                    placeholder="Selected Doctor"
+                    editable = {false}
+                    value = {appointmentModel.doctor.fullName}
 
+                />
+      
             <Text style={styles.titleText}>Select Service</Text>
             <View  style={styles.pickerstyle}>
+
             <Picker
             selectedValue={selectedValue}
            
@@ -25,8 +38,10 @@ export const ConfirmAppointmentScreen: React.FC<TProps> = props => {
             <Picker.Item label="Java" value="java" />
             <Picker.Item label="JavaScript" value="js" />
           </Picker>
+            </View>
+            <Text style={styles.titleText}>Service Charge</Text>
 
-          <TextInput
+            <TextInput
                     style={styles.input}
                     placeholder="Amount"
                 />
@@ -36,7 +51,6 @@ export const ConfirmAppointmentScreen: React.FC<TProps> = props => {
                   type="outline"
                     style={styles.buttonStyle}
                 />
-            </View>
 
         </View>
       );
@@ -46,13 +60,13 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
      
-    paddingLeft:10
+    padding:10
     },
     titleText: {
         fontSize: 17,
         fontWeight: "600",
         color: Theme.colors.black,
-        paddingTop:20
+        margin:10
       },
     pickerstyle:{
         height: 50, 
@@ -60,12 +74,11 @@ const styles = StyleSheet.create({
         borderWidth:1,
         borderColor: Theme.colors.primaryColor,
         borderRadius:5,
-        marginTop:10,
-        marginRight:10
+        
+      
     },
     buttonStyle:{
-        marginTop:30,
-       
+        marginTop:30,       
         alignSelf: 'stretch',
         fontSize: 20,
         height: 50
@@ -76,6 +89,7 @@ const styles = StyleSheet.create({
         borderWidth:0.5,
         borderColor: Theme.colors.primaryColor,
         padding:10,
-        marginTop:30
+        marginTop:10
+      
             },
   });
