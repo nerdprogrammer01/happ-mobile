025e03d764aff4df0e3463a2947ef29c2a97994d
CNwadike <chimanwadike94@gmail.com>
2020-06-23 01:14:26 +0100

    payment updates

--- src/screens/appointment/ConfirmAppointmentScreen.tsx
+++ src/screens/appointment/ConfirmAppointmentScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Picker, StyleSheet, TextInput } from 'react-native';
+import { View, Text, Picker, StyleSheet, TextInput, AsyncStorage } from 'react-native';
 import { Theme } from "../../theme";
 import { Button } from "../../components/buttons";
 import { useLocalization } from "../../localization";
@@ -19,7 +19,7 @@ import { CreditCardInput, LiteCreditCardInput } from "react-native-credit-card-i
 type TProps = {};
 
 export const ConfirmAppointmentScreen: React.FC<TProps> = props => {
-  const [serviceId, setServiceId] = useState(0);
+  const [serviceId, setServiceId] = useState("");
   const navigation = useNavigation();
   const route = useRoute()
   const { getString } = useLocalization();
@@ -29,8 +29,48 @@ export const ConfirmAppointmentScreen: React.FC<TProps> = props => {
   const [appointmentDate, setAppointmentDate] = useState(new Date());
   const [appointmentTime, setAppointmentTime] = useState("");
   const [validDebitCard, setValidDebitCard] = useState(false)
+  const [servicePeriod, setServicePeriod] = useState(0)
+  const [start_date, setStart_date] = useState(null)
+  const [end_date, setEnd_date] = useState(null)
+  const [profile, setProfile] = useState(null);
+  const [transRef, setTransRef] = useState("");
+  const [cardInfo, setCardInfo] = useState(null);
+
+    //datepicker related 
+  const [date, setDate] = useState("");
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  
+    const showDatePicker = () => {
+      setDatePickerVisibility(true);
+    };
+  
+    const hideDatePicker = () => {
+      setDatePickerVisibility(false);
+    };
+  
+    const handleConfirm = (date: Date) => {
+      let datestring = Moment(date).format('DD-MM-YYYY')
+      setDate(datestring);
+//      setAppointmentDate(date)
+      hideDatePicker();
+    };
+
 
   let appointmentModel = JSON.parse(route.params["appointmentModel"]) as NewAppointmentModel;
+
+  useEffect(() => {
+    async function load_profile() {
+      await AsyncStorage.getItem('profile')
+      .then((data) => {
+        setProfile(JSON.parse(data));
+    })
+    .catch((err) => {
+       console.log(err);
+    });   
+    }
+   load_profile();
+  }, []);
+  
 
   const getClinicianServices = () => {
     let request = {
@@ -54,9 +94,15 @@ export const ConfirmAppointmentScreen: React.FC<TProps> = props => {
       });
   }
 
-  const getServiceCost = (service_id: number) => {
+  const getServiceCost = (service_id: string) => {
 
     setServiceId(service_id)
+
+    doctorServices.filter(function(service) {
+      if(service.id == service_id){      
+        return setServicePeriod(service.time_minutes)
+      } 
+    })
 
     let request = {
       method: "GET",
@@ -72,7 +118,6 @@ export const ConfirmAppointmentScreen: React.FC<TProps> = props => {
         return response.json();
       })
       .then(responseJson => {
-        console.log(responseJson.cost);
         setAmount(responseJson.cost)
       })
       .catch(error => {
@@ -80,44 +125,98 @@ export const ConfirmAppointmentScreen: React.FC<TProps> = props => {
       });
   }
 
-  const checkAvailability = () => {
+  const checkAvailability = (timeString : string) => {
+    setAppointmentTime(timeString);
     setAvailable(!available)
+
+    if(date != null && servicePeriod > 0){
+      var start_date = Moment(date.trim()+" "+appointmentTime.trim()+":00", 'DD-MM-YYYY hh:mm:ss').format('DD-MM-YYYY hh:mm:ss')
+      var end_date = Moment(start_date,'DD-MM-YYYY hh:mm').add(servicePeriod, 'minutes').format('DD-MM-YYYY hh:mm:ss');
+      
+      setStart_date(start_date)
+      setEnd_date(end_date)
+    }
+  }
+
+  const postAppointment = () => {
+    let requestBody = JSON.stringify({
+      client_id: profile.id,
+      clinician_id: appointmentModel.doctor.id,
+      status:0,
+      appointment_type:appointmentModel.appointmentType,
+      start_date : start_date,
+      end_date : end_date,
+      created_at : new Date(),
+      appointment_serivice:serviceId,
+      appointment_activity_id:appointmentModel.appointmentCategory,
+      appointment_activity_sub_id:appointmentModel.appointmentActivity,
+      cancel_reason:""
+
+    });
+
+    let request = {
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json",
+        //'Token': profile.token
+      },
+      body:requestBody
+    };
+
+    fetch(Environment.SERVER_API + "/api/appointment/PostAppointment", request)
+      .then((response) => {
+        JSON.stringify(response, null, 4)
+        return response.json();
+      })
+      .then(responseJson => {
+        setTransRef(responseJson)
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   const paymentFormHandler = (form) => {
-    if (form.valid)
+    if (form.valid){
       setValidDebitCard(form.valid)
+      setCardInfo(form)
+    }
+
   }
 
   const pay = () =>
   {
-    RNPaystack.init({ publicKey: 'hellllo' })   
-    alert('i done pay')
+    if(validDebitCard && cardInfo !=null){
+      {postAppointment}
 
+      if(transRef.length > 0){
+        RNPaystack.init({ publicKey: Environment.PAYSTACK_PUBLIC_KEY })
+        RNPaystack.chargeCardWithAccessCode({
+          cardNumber: cardInfo.values.number, 
+          expiryMonth: cardInfo.values.expiry, 
+          expiryYear: cardInfo.values.number, 
+          cvc: cardInfo.values.cvc,
+          accessCode: transRef,
+          
+        })
+      .then(response => {
+        console.log(response); // do stuff with the token
+        //confirm payment here
+      })
+      .catch(error => {
+        console.log(error); // error is a javascript Error object
+        console.log(error.message);
+        console.log(error.code);
+      })   
+      }
+    }
   }
 
   useEffect(() => {
     getClinicianServices();
   }, []);
 
-  //datepicker related 
-  const [date, setDate] = useState("");
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
-  };
-
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
-  };
-
-  const handleConfirm = (date: Date) => {
-    let datestring = Moment(date).format('DD-MM-YYYY')
-    setDate(datestring);
-    setAppointmentDate(date)
-    hideDatePicker();
-  };
 
   return (
     <ScrollView style={styles.container}>
@@ -158,7 +257,7 @@ export const ConfirmAppointmentScreen: React.FC<TProps> = props => {
             value={date}
           />
           <View style={styles.pickerstyle2}>
-            <Picker onValueChange={checkAvailability}>
+            <Picker onValueChange={(itemValue) => checkAvailability(itemValue)}>
               <Picker.Item label="Select Time" value="Select Time" />
               {Times.map((item, key) => (
                 <Picker.Item label={item} value={item} key={key} />)
